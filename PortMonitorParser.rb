670659ad6f28d937f1021de170e5dda8d4ae6d5f
Antonio Miguel Pozo Cámara <apozo@MacBook-Pro-de-Antonio.local>
2017-06-02 20:08:56 +0200

    Model defined

---
+++ PortMonitorParser.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'nokogiri'
+
+
+class Product
+
+
+  attr_reader rate
+  attr_reader history
+  attr_reader multiple_email_notifications
+  attr_reader push_notifications
+  attr_reader monthly_price
+  attr_reader annual_price
+
+
+end
